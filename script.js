15bc9633ad9ff269766b55aeb465e9a473f33c4c
marcinmagala <marcinmagala@gmail.com>
2022-08-31 17:12:51 +0200

    change place in array

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-"use strict";
+'use strict';
+
+// Zamiana miejscami elementów tablicy
+
+// let arr1 = [1, 2, 3, 4, 5];
+// let arr2 = ['a', 'b', 'c', 'd', 'e'];
+// let arr3 = [];
+
+// const changePlace = function (arr) {
+//   if (arr.length > 1) {
+//     arr = [arr.at(-1), ...arr.slice(1, -1), arr[0]];
+//   } else {
+//     arr;
+//   }
+
+//   console.log(arr);
+// };
+
+// const changePlace = arr =>
+//   arr.length > 1
+//     ? console.log((arr = [arr.at(-1), ...arr.slice(1, -1), arr[0]]))
+//     : console.log(arr);
+
+// changePlace(arr1);
+// changePlace(arr2);
+// changePlace(arr3);
+
+// ######################################
+// ######################################
+// ######################################
